5ce462181b29e3718157eec95fbd9c242d44c99e
Yash Raj <yashraj.se10@gmail.com>
2023-06-03 18:10:28 +0530

    CRUD Done

--- controllers/task.js
+++ controllers/task.js
@@ -15,3 +15,40 @@ export const newTask = async (req, res, next)=>{
     });
 
 };
+
+export const getMyTask = async (req, res, next )=>{
+    const userid = req.user._id;
+
+    const tasks = await Task.find({ user: userid });
+
+    res.status(200).json({
+        sucess:true,
+        tasks,
+    });
+}  ;
+
+
+export const upadateTask = async (req, res, next )=>{
+    const task = await Task.findById(req.params.id);
+
+    task.isCompleated = !task.isCompleated;
+
+    await task.save();
+
+    res.status(200).json({
+        sucess:true,
+        message: "task upadated!",
+    });
+}  ;
+
+export const deleatTask = async (req, res, next )=>{
+    const task = await Task.findById(req.params.id);
+
+    await task.deleteOne();
+
+
+    res.status(200).json({
+        sucess:true,
+        message: "task deleted!",
+    });
+}  ;
--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { newTask } from "../controllers/task.js"
+import { deleatTask, getMyTask, newTask, upadateTask } from "../controllers/task.js"
 import { isAuth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/new", isAuth, newTask);
+
+router.post("/my", isAuth, getMyTask);
+
+router.route("/:id").put(isAuth, upadateTask).delete(isAuth, deleatTask);
 
 export default router
